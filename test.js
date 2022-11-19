d84538e00796bd3faeac46b1759a16b7b3ca294f
WenNbiz <41163558+webnbiz@users.noreply.github.com>
2022-11-19 17:10:12 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,15 +1,24 @@
-let express = require("expess")
+let express = require("express")
 let ourApp = express()
 ourApp.get('/', function(req, res){
 
  res.send(`
- <form>
-//  <p>What color?</p>
+
+ <form action="/answer" method="POST">
+ <p>What colorrrr?</p>
  <input name="skyColor" autocomplete="off"></input>
- <button></button>
+ <button>Submit answer</button>
  </form>
  
  `)
+
+ ourApp.post('/answer', function(req, res){
+    res.send("thank you")
+ })
+
+ ourApp.get('/answer', function(req, res){
+    res.send("Lost")
+ })
 
 })
 
